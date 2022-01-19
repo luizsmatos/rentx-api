fbf2baf92e9c18d7b87ace56c6c8c0f986b572b0
luizsmatos <gusstavopdm@gmail.com>
2022-01-19 10:47:46 -0300

    feature: UseCases importCategory

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRouters = Router();
+const upload = multer({ dest: './tmp' });
 
 categoriesRouters.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -12,5 +15,13 @@ categoriesRouters.post('/', (request, response) => {
 categoriesRouters.get('/', (request, response) => {
   return listCategoriesController.handle(request, response);
 });
+
+categoriesRouters.post(
+  '/import',
+  upload.single('file'),
+  (request, response) => {
+    return importCategoryController.handle(request, response);
+  },
+);
 
 export { categoriesRouters };
