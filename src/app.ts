bea6d1dd234200248dec4a89b663d6e835d2d3fa
Luiz Gustavo <gusstavopdm@gmail.com>
2022-07-07 08:27:50 -0300

    feat(app/server): adicionando app

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import '@shared/infra/typeorm';
+
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import { router } from './routes';
+import swaggerFile from './swagger.json';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(router);
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,8 @@
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
+import { createConnection } from '@shared/infra/typeorm';
 
-import { router } from './routes';
-import swaggerFile from './swagger.json';
+import app from './app';
 
-const app = express();
-
-app.use(express.json());
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(router);
+createConnection();
 
 app.listen(3333, () => {
   console.log('Server is running on port 3333');
