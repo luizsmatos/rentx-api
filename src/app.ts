bc1f7f211c472383b2ae3d4f4bb2da29319414e8
Luiz Gustavo <gusstavopdm@gmail.com>
2022-07-07 09:47:28 -0300

    feat(Category): adicionando container Category

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
+import '@shared/container';
 
 import { router } from './routes';
 import swaggerFile from './swagger.json';
