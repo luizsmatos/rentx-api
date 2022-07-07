dddc5175e6142242edfec0675414f7754edd52bb
Luiz Gustavo <gusstavopdm@gmail.com>
2022-07-07 08:32:19 -0300

    refactor(Categories/useCases): remove metodo getInstance

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,26 +1,26 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import { createCategoryController } from '../modules/cars/useCases/createCategory';
-import { importCategoryController } from '../modules/cars/useCases/importCategory';
-import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import createCategoryController from '../modules/cars/useCases/createCategory';
+import importCategoryController from '../modules/cars/useCases/importCategory';
+import listCategoriesController from '../modules/cars/useCases/listCategories';
 
 const categoriesRouters = Router();
 const upload = multer({ dest: './tmp' });
 
 categoriesRouters.post('/', (request, response) => {
-  return createCategoryController.handle(request, response);
+  return createCategoryController().handle(request, response);
 });
 
 categoriesRouters.get('/', (request, response) => {
-  return listCategoriesController.handle(request, response);
+  return listCategoriesController().handle(request, response);
 });
 
 categoriesRouters.post(
   '/import',
   upload.single('file'),
   (request, response) => {
-    return importCategoryController.handle(request, response);
+    return importCategoryController().handle(request, response);
   },
 );
 
