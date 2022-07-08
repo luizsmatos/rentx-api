9c1414a35e661d8d7f80db933846f7953bd699fe
Luiz Gustavo <gusstavopdm@gmail.com>
2022-07-08 16:56:43 -0300

    feat(typeorm): adicionando migration CreateSpecification

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,9 +2,11 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import { Category } from '@modules/cars/entities/Category';
+import { Specification } from '@modules/cars/entities/Specification';
 import { DataSource } from 'typeorm';
 
 import { CreateCategories1657024580247 } from './migrations/1657024580247-CreateCategories';
+import { CreateSpecifications1657309295662 } from './migrations/1657309295662-CreateSpecifications';
 
 const dataSource = new DataSource({
   type: 'postgres',
@@ -14,8 +16,11 @@ const dataSource = new DataSource({
   database: process.env.DB_NAME,
   synchronize: false,
   logging: false,
-  migrations: [CreateCategories1657024580247],
-  entities: [Category],
+  migrations: [
+    CreateCategories1657024580247,
+    CreateSpecifications1657309295662,
+  ],
+  entities: [Category, Specification],
   subscribers: [],
 });
 
