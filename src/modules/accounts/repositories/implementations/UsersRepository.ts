117082e28ca4b9d14234ca6865921c8a44906637
Luiz Gustavo <gusstavopdm@gmail.com>
2022-07-11 10:12:18 -0300

    feat(User): adicionando UsersRepository e IUsersRepository

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,35 @@
+import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDTO';
+import { User } from '@modules/accounts/entities/User';
+import { Repository } from 'typeorm';
+
+import dataSource from '@shared/infra/typeorm';
+
+import { IUsersRepository } from '../IUsersRepository';
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = dataSource.getRepository(User);
+  }
+
+  async create({
+    name,
+    username,
+    password,
+    email,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      username,
+      password,
+      email,
+      driver_license,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UsersRepository };
