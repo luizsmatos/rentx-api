a651a30d0c8665d1b81fe9fed619e8c41d22b1db
Luiz Gustavo <gusstavopdm@gmail.com>
2022-07-11 23:42:57 -0300

    feat(User): adicionando authenticateUser

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,55 @@
+import { UsersRepository } from '@modules/accounts/repositories/implementations/UsersRepository';
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: UsersRepository,
+  ) {}
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error(`Email or password incorrect`);
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error(`Email or password incorrect`);
+    }
+
+    const token = sign({}, process.env.JWT_SECRET_USER, {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    const tokenReturn: IResponse = {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
