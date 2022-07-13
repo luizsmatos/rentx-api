4499373554ff7c3128c8c9d990f336b6bd22d5bb
Luiz Gustavo <gusstavopdm@gmail.com>
2022-07-13 08:24:42 -0300

    feat(app): adicionando middleware de error

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import { AppError } from '@errors/AppError';
+import express, { Request, Response, NextFunction } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import '@shared/container';
+import 'express-async-errors';
 
 import { router } from './routes';
 import swaggerFile from './swagger.json';
@@ -11,5 +13,18 @@ app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
+
+app.use(
+  (err: Error, _request: Request, response: Response, _next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({ message: err.message });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  },
+);
 
 export default app;
