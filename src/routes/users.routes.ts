161c25673629d1ec6562987d80e087775067fd2c
Luiz Gustavo <gusstavopdm@gmail.com>
2022-07-13 10:01:42 -0300

    feat(User): adicionando rota /users/avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,23 @@
+import uploadConfig from '@config/upload';
+import { ensureAuthenticate } from '@middlewares/ensureAuthenticate';
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 import { Router } from 'express';
+import multer from 'multer';
 
 const usersRouters = Router();
 
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRouters.post('/', createUserController.handle);
+usersRouters.patch(
+  '/avatar',
+  ensureAuthenticate,
+  uploadAvatar.single('avatar'),
+  updateUserAvatarController.handle,
+);
 
 export { usersRouters };
