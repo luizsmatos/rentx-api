c1be2fbbf1d3b20d73c9336570945908fbc9292a
Luiz Gustavo <gusstavopdm@gmail.com>
2022-07-18 09:15:00 -0300

    feat(routes/categories): adicionando ensureAutheticate e ensureAdmin

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import multer from 'multer';
 
+import { ensureAdmin } from '@middlewares/ensureAdmin';
+import { ensureAuthenticate } from '@middlewares/ensureAuthenticate';
 import { CreateCategoryController } from '@modules/cars/useCases/createCategory/CreateCategoryController';
 import { ImportCategoryController } from '@modules/cars/useCases/importCategory/ImportCategoryController';
 import { ListCategoriesController } from '@modules/cars/useCases/listCategories/ListCategoriesController';
@@ -12,7 +14,12 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRouters.post('/', createCategoryController.handle);
+categoriesRouters.post(
+  '/',
+  ensureAuthenticate,
+  ensureAdmin,
+  createCategoryController.handle,
+);
 
 categoriesRouters.get('/', listCategoriesController.handle);
 
