f15537bdbdd3784109c4f4246d3db0f179c49aba
Luiz Gustavo <gusstavopdm@gmail.com>
2022-07-18 08:20:44 -0300

    feat(routes): adicionando routes /cars

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { authenticateRouters } from './authenticate.routes';
+import { carsRoutes } from './cars.routes';
 import { categoriesRouters } from './categories.routes';
 import { specificationsRouters } from './specifications.routes';
 import { usersRouters } from './users.routes';
@@ -10,6 +11,7 @@ const router = Router();
 router.use('/categories', categoriesRouters);
 router.use('/specifications', specificationsRouters);
 router.use('/users', usersRouters);
+router.use('/cars', carsRoutes);
 router.use(authenticateRouters);
 
 export { router };
