673393daa167cedde16ab53d548a20b29dc2256c
Luiz Gustavo <gusstavopdm@gmail.com>
2022-07-19 09:15:13 -0300

    feat(Cars): adicionando routes /cars/available

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import { ensureAdmin } from '@middlewares/ensureAdmin';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 import { ensureAuthenticate } from '@shared/infra/http/middlewares/ensureAuthenticate';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.post(
   '/',
@@ -14,5 +16,7 @@ carsRoutes.post(
   ensureAdmin,
   createCarController.handle,
 );
+
+carsRoutes.get('/available', listAvailableCarsController.handle);
 
 export { carsRoutes };
