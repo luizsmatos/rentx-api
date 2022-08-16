e72d641d1d5fc6d515d2eb00b9844ee469aaea53
Luiz Gustavo <gusstavopdm@gmail.com>
2022-08-16 09:27:33 -0300

    feat(SpecificationsRepositoryInMemory): implementa findByIds

--- src/modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory.ts
@@ -8,19 +8,33 @@ import {
 class SpecificationsRepositoryInMemory implements ISpecificationsRepository {
   specifications: Specification[] = [];
 
-  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+  async create({
+    name,
+    description,
+  }: ICreateSpecificationDTO): Promise<Specification> {
     const specification = new Specification();
 
     Object.assign(specification, { name, description });
 
     this.specifications.push(specification);
+
+    return specification;
   }
+
   async findByName(name: string): Promise<Specification> {
     const specification = this.specifications.find(
       (specification) => specification.name === name,
     );
 
     return specification;
+  }
+
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    const specifications = this.specifications.filter((specification) =>
+      ids.includes(specification.id),
+    );
+
+    return specifications;
   }
 }
 
