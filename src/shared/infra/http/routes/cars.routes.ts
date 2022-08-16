829e35dfb44b28aa0d0aeaff426c622a5fdae64d
Luiz Gustavo <gusstavopdm@gmail.com>
2022-08-16 09:23:50 -0300

    feat(carsRoutes): adicionando /specifications/:id

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { ensureAdmin } from '@middlewares/ensureAdmin';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
 import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 import { ensureAuthenticate } from '@shared/infra/http/middlewares/ensureAuthenticate';
 
@@ -9,6 +10,7 @@ const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 carsRoutes.post(
   '/',
@@ -18,5 +20,12 @@ carsRoutes.post(
 );
 
 carsRoutes.get('/available', listAvailableCarsController.handle);
+
+carsRoutes.post(
+  '/specifications/:id',
+  ensureAuthenticate,
+  ensureAdmin,
+  createCarSpecificationController.handle,
+);
 
 export { carsRoutes };
