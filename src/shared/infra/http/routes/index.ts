9ac3a7a8ea136b38c34312b7f1694127522b75ca
Luiz Gustavo <gusstavopdm@gmail.com>
2022-08-19 15:28:50 -0300

    feat(routes): import rentalRoutes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,17 +1,19 @@
 import { Router } from 'express';
 
-import { authenticateRouters } from './authenticate.routes';
+import { authenticateRoutes } from './authenticate.routes';
 import { carsRoutes } from './cars.routes';
-import { categoriesRouters } from './categories.routes';
-import { specificationsRouters } from './specifications.routes';
-import { usersRouters } from './users.routes';
+import { categoriesRoutes } from './categories.routes';
+import { rentalRoutes } from './rental.routes';
+import { specificationsRoutes } from './specifications.routes';
+import { usersRoutes } from './users.routes';
 
 const router = Router();
 
-router.use('/categories', categoriesRouters);
-router.use('/specifications', specificationsRouters);
-router.use('/users', usersRouters);
+router.use('/categories', categoriesRoutes);
+router.use('/specifications', specificationsRoutes);
+router.use('/users', usersRoutes);
 router.use('/cars', carsRoutes);
-router.use(authenticateRouters);
+router.use('/rentals', rentalRoutes);
+router.use(authenticateRoutes);
 
 export { router };
