31a339667543cbf3f40dbbfccab5da1c5079b333
Luiz Gustavo <gusstavopdm@gmail.com>
2022-08-30 10:05:34 -0300

    feat(IRentalsRepository): adicionando findByUser

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -3,9 +3,10 @@ import { Rental } from '../infra/typeorm/entities/Rental';
 
 interface IRentalsRepository {
   create(data: ICreateRentalDTO): Promise<Rental>;
-  findRentalByCar(car_id: string): Promise<Rental>;
-  findRentalByUser(user_id: string): Promise<Rental>;
+  findOpenRentalByCar(car_id: string): Promise<Rental>;
+  findOpenRentalByUser(user_id: string): Promise<Rental>;
   findById(id: string): Promise<Rental>;
+  findByUser(user_id: string): Promise<Rental[]>;
 }
 
 export { IRentalsRepository };
