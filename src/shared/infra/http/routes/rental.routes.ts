a94ec51906abf5e35d17933936aef3de3b2798f0
Luiz Gustavo <gusstavopdm@gmail.com>
2022-08-30 09:19:35 -0300

    feat(rentalRoutes): adicionando /devolution/:id

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -2,11 +2,18 @@ import { Router } from 'express';
 
 import { ensureAuthenticate } from '@middlewares/ensureAuthenticate';
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
+import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalRoutes.post('/', ensureAuthenticate, createRentalController.handle);
+rentalRoutes.post(
+  '/devolution/:id',
+  ensureAuthenticate,
+  devolutionRentalController.handle,
+);
 
 export { rentalRoutes };
