edbb7205dca6b156fb4b74ca8ae6d46463ef35f6
Luiz Gustavo <gusstavopdm@gmail.com>
2022-08-30 09:18:31 -0300

    feat(RentalsRepositoryInMemory): implementando findById

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -42,6 +42,12 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
 
     return rental;
   }
+
+  async findById(id: string): Promise<Rental> {
+    const rental = this.rentals.find((rental) => rental.id === id);
+
+    return rental;
+  }
 }
 
 export { RentalsRepositoryInMemory };
