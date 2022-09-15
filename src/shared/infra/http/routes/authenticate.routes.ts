e5c5a245efa9c26d25f159a4aec353b7401d9e87
Luiz Gustavo <gusstavopdm@gmail.com>
2022-09-15 17:06:06 -0300

    feat(authenticateRoutes): adicionando /refresh_token

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { AuthenticateUserController } from '@modules/accounts/useCases/authenticateUser/AuthenticateUserController';
+import { RefreshTokenController } from '@modules/accounts/useCases/refreshToken/RefrashTokenController';
 
 const authenticateRoutes = Router();
 
-const createUserController = new AuthenticateUserController();
+const authenticateUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
 
-authenticateRoutes.post('/sessions', createUserController.handle);
+authenticateRoutes.post('/sessions', authenticateUserController.handle);
+authenticateRoutes.post('/refresh_token', refreshTokenController.handle);
 
 export { authenticateRoutes };
