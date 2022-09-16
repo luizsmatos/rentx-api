26fa19bb6739a8cee1a38c071f77b130a763c390
Luiz Gustavo <gusstavopdm@gmail.com>
2022-09-16 12:58:17 -0300

    feat(MailProvider): adicionando EtherealMailProvider

---
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -0,0 +1,53 @@
+/* eslint-disable consistent-return */
+import fs from 'fs';
+import handlebars from 'handlebars';
+import nodemailer, { Transporter } from 'nodemailer';
+
+import { IMailProvider } from '../IMailProvider';
+
+class EtherealMailProvider implements IMailProvider {
+  private client: Transporter;
+  constructor() {
+    nodemailer
+      .createTestAccount()
+      .then((account) => {
+        const transporter = nodemailer.createTransport({
+          host: account.smtp.host,
+          port: account.smtp.port,
+          secure: account.smtp.secure,
+          auth: {
+            user: account.user,
+            pass: account.pass,
+          },
+        });
+
+        this.client = transporter;
+      })
+      .catch((error) => console.error(error));
+  }
+
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: unknown,
+    path: string,
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path, 'utf8');
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
+    const message = await this.client.sendMail({
+      to,
+      from: 'Rentx <noreplay@rentx.com.br>',
+      subject,
+      html: templateHTML,
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
+
+export { EtherealMailProvider };
