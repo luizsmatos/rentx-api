0709514e66999e2fc1054489ad634d83a0b7d1b7
Luiz Gustavo <gusstavopdm@gmail.com>
2022-09-16 14:35:34 -0300

    feat(IDateProvider): adicionando compareIsBefore

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -5,6 +5,7 @@ interface IDateProvider {
   dateNow(): Date;
   addDays(days: number): Date;
   addHours(hours: number): Date;
+  compareIsBefore(start_date: Date, end_date: Date): boolean;
 }
 
 export { IDateProvider };
