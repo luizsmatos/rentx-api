273ae799c0feea3b55b5d669054e380a012e3e43
Luiz Gustavo <gusstavopdm@gmail.com>
2022-09-26 17:36:04 -0300

    feat(UploadCarImagesUseCase): adicionando storageProvider

--- src/modules/cars/useCases/uploadCarImagem/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImagem/UploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import { CarsImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository';
+import { IStorageProvider } from '@shared/container/providers/StorageProvider/IStorageProvider';
 
 interface IRequest {
   car_id: string;
@@ -12,10 +13,14 @@ class UploadCarImagesUseCase {
   constructor(
     @inject('CarsImagesRepository')
     private carsImagesRepository: CarsImagesRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
+
   async execute({ car_id, images_name }: IRequest): Promise<void> {
-    images_name.map(async (image) => {
+    images_name.forEach(async (image) => {
       await this.carsImagesRepository.create(car_id, image);
+      await this.storageProvider.save(image, 'cars');
     });
   }
 }
