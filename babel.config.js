db52d46079aa1ad601c210ffc87f32a31e3d09f8
Luiz Gustavo <gusstavopdm@gmail.com>
2022-10-10 13:15:02 -0300

    feat: adicionando babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@modules': ['./src/modules'],
+          '@middlewares': ['src/shared/infra/http/middlewares'],
+          '@config': ['./src/config'],
+          '@shared': ['./src/shared'],
+          '@errors': ['src/shared/errors'],
+          '@utils': ['./src/utils'],
+        },
+      },
+    ],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+};
